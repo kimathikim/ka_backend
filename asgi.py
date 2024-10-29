80f2a2f72f694ae67e19d40209aa218add3188d6
kimathikim <briankimathi94@gmail.com>
2024-10-30 02:06:41 +0400

    update readme

--- asgi.py
+++ asgi.py
@@ -16,11 +16,8 @@ db = client.get_default_database()
 app.register_blueprint(auth_bp, url_prefix="/auth")
 app.register_blueprint(contact_bp, url_prefix="/contact")
 
-asgi_app = WsgiToAsgi(app)
 
 if __name__ == "__main__":
-    import uvicorn
-
     # Configure logging
     logging.basicConfig(level=logging.INFO)
-    uvicorn.run(asgi_app)
+    asgi_app = WsgiToAsgi(app)
